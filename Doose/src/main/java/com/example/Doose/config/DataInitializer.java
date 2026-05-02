56ec5d2557201a8d3c63ffe5507c7c427a8c116c
Duan <dgomez@wesportech.com>
2026-05-02 15:43:33 -0500

    first commit

--- Doose/src/main/java/com/example/Doose/config/DataInitializer.java
+++ Doose/src/main/java/com/example/Doose/config/DataInitializer.java
@@ -1,9 +1,13 @@
 package com.example.Doose.config;
 
 import com.example.Doose.model.User;
+import com.example.Doose.model.TattooService;
 import com.example.Doose.store.DataStore;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+
+import java.security.Provider.Service;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -38,5 +42,17 @@ public class DataInitializer {
                 .createdAt(java.time.LocalDateTime.now())
                 .build();
         dataStore.users.add(userDuan);
+    }
+
+    @PostConstruct
+    public void initService(){
+        TattooService service1 = TattooService.builder()
+                .id(dataStore.nextServiceId())
+                .name("Traditional Tattoo")
+                .description("Classic designs with bold lines and vibrant colors.")
+                .price(150.0)
+                .createdAt(java.time.LocalDateTime.now())
+                .build();
+        dataStore.services.add(service1);
     }
 }
